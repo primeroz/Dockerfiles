178b14d761a07657ca1c02c869a194606259bfed
PrZ <prz@prznix.home>
2017-07-01 14:46:10 +0100

    Update

--- monero-go-pool/monero-wallet-rpc.sh
+++ monero-go-pool/monero-wallet-rpc.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [[ -z "${TESTNET}" ]]; then
-	exec /sbin/setuser root /usr/local/src/monero/bin/monero-wallet-rpc --trusted-daemon --wallet-file /monero/$WALLET_FILE --password $WALLET_PASSWORD --rpc-bind-port=$WALLET_RPC_PORT --disable-rpc-login --rpc-bind-ip=$WALLET_RPC_IP 2>&1
+				exec /sbin/setuser root /usr/local/src/monero/bin/monero-wallet-rpc --trusted-daemon --wallet-file /monero/$WALLET_FILE --password $WALLET_PASSWORD --rpc-bind-port=$WALLET_RPC_BIND_PORT --disable-rpc-login --rpc-bind-ip=$WALLET_RPC_BIND_IP 2>&1
 
 else
-	exec /sbin/setuser root /usr/local/src/monero/bin/monero-wallet-rpc --testnet --trusted-daemon --wallet-file /monero/$WALLET_FILE --password $WALLET_PASSWORD --rpc-bind-port=$WALLET_RPC_PORT --disable-rpc-login --rpc-bind-ip=$WALLET_RPC_IP 2>&1
+				exec /sbin/setuser root /usr/local/src/monero/bin/monero-wallet-rpc --testnet --trusted-daemon --wallet-file /monero/$WALLET_FILE --password $WALLET_PASSWORD --rpc-bind-port=$WALLET_RPC_BIND_PORT --disable-rpc-login --rpc-bind-ip=$WALLET_RPC_BIND_IP 2>&1
 fi
 
