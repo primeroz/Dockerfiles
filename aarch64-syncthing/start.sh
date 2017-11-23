537dd545cd4d349a8a2272291c65fc506ef85df1
Francesco Ciocchetti <primeroznl@gmail.com>
2017-11-23 19:54:36 +0000

    Add arm64 syncthing image

---
+++ aarch64-syncthing/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+# if this if the first run, generate a useful config
+if [ ! -f /srv/config/config.xml ]; then
+  echo "generating config"
+  /srv/syncthing/syncthing --generate="/srv/config"
+  # don't take the whole volume with the default so that we can add additional folders
+  sed -e 's/path="\/root\/Sync\/"/path="\/srv\/data\/default"/' -i /srv/config/config.xml
+  # ensure we can see the web ui outside of the docker container
+  sed -e "s/<address>127.0.0.1:8384/<address>0.0.0.0:8080/" -i /srv/config/config.xml
+fi
+
+usermod -u $UID syncthing
+# set permissions so that we have access to volumes
+chown -R syncthing:users /srv/config /srv/data /srv/syncthing
+chmod -R 770 /srv/config /srv/data
+
+gosu syncthing /srv/syncthing/syncthing -home=/srv/config
+
